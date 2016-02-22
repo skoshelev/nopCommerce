3e87f2b3ea47fc8ff9e55015af8316cdcce2da4a
poyker <boyko.stanev@gmail.com>
2016-02-22 16:54:28 +0200

    Created Nop.Plugin.Api plugin project and added it to the nopCommerce solution

---
+++ src/Plugins/Nop.Plugin.Api/Plugin/ApiPlugin.cs
@@ -0,0 +1,29 @@
+﻿using Nop.Core.Plugins;
+using Nop.Web.Framework.Menu;
+using Nop.Services.Localization;
+
+namespace Nop.Plugin.Api.Plugin
+{
+    public class ApiPlugin : BasePlugin, IAdminMenuPlugin
+    {
+        public override void Install()
+        {
+            //locales
+            this.AddOrUpdatePluginLocaleResource("Plugins.Api", "Api plugin");
+           
+            base.Install();
+        }
+
+        public override void Uninstall()
+        {
+            //locales
+            this.DeletePluginLocaleResource("Plugins.Api");
+            base.Uninstall();
+        }
+
+        public void ManageSiteMap(SiteMapNode rootNode)
+        {
+           
+        }
+    }
+}
