65ce72497071f538b0177b5a22533be807bf2192
itanev <iliyan.tanev@gmail.com>
2016-02-23 14:50:13 +0200

    quit
    sadf
    
    t
    quit

--- src/Plugins/Nop.Plugin.Api/Plugin/ApiPlugin.cs
+++ src/Plugins/Nop.Plugin.Api/Plugin/ApiPlugin.cs
@@ -1,16 +1,23 @@
-﻿using Nop.Core.Plugins;
+﻿using System.Web.Routing;
+using Nop.Core.Plugins;
+using Nop.Plugin.Api.MVC;
+using Nop.Services.Common;
 using Nop.Web.Framework.Menu;
 using Nop.Services.Localization;
 
 namespace Nop.Plugin.Api.Plugin
 {
-    public class ApiPlugin : BasePlugin, IAdminMenuPlugin
+    public class ApiPlugin : BasePlugin, IAdminMenuPlugin, IMiscPlugin
     {
         public override void Install()
         {
             //locales
             this.AddOrUpdatePluginLocaleResource("Plugins.Api", "Api plugin");
-           
+            this.AddOrUpdatePluginLocaleResource("Plugins.Api.Admin.Configure", "Configure Web API");
+            this.AddOrUpdatePluginLocaleResource("Plugins.Api.Admin.GeneralSettings", "General Settings");
+            this.AddOrUpdatePluginLocaleResource("Plugins.Api.Admin.EnableApi", "Enable Api");
+            this.AddOrUpdatePluginLocaleResource("Plugins.Api.Admin.EnableApi.Hint", "By checking this settings you can Enable/Disable the Web Api");
+
             base.Install();
         }
 
@@ -24,6 +31,19 @@ namespace Nop.Plugin.Api.Plugin
         public void ManageSiteMap(SiteMapNode rootNode)
         {
            
+        }
+
+        /// <summary>
+        /// Gets a route for provider configuration
+        /// </summary>
+        /// <param name="actionName">Action name</param>
+        /// <param name="controllerName">Controller name</param>
+        /// <param name="routeValues">Route values</param>
+        public void GetConfigurationRoute(out string actionName, out string controllerName, out RouteValueDictionary routeValues)
+        {
+            actionName = "Configure";
+            controllerName = "ApiAdmin";
+            routeValues = new RouteValueDictionary { { "Namespaces", PluginNames.ControllersNamespace }, { "area", null } };
         }
     }
 }
