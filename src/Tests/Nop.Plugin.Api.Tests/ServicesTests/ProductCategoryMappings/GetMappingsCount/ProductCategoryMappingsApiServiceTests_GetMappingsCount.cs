a080b44e7e7cda87559012b13d587a5fe017b91b
itanev <iliyan.tanev@gmail.com>
2016-06-02 11:22:07 +0300

    - Add a setup method, - Made sure that all the items that we are testing for will be 100% there.

--- src/Tests/Nop.Plugin.Api.Tests/ServicesTests/ProductCategoryMappings/GetMappingsCount/ProductCategoryMappingsApiServiceTests_GetMappingsCount.cs
+++ src/Tests/Nop.Plugin.Api.Tests/ServicesTests/ProductCategoryMappings/GetMappingsCount/ProductCategoryMappingsApiServiceTests_GetMappingsCount.cs
@@ -12,27 +12,51 @@ namespace Nop.Plugin.Api.Tests.ServicesTests.ProductCategoryMappings.GetMappings
     [TestFixture]
     public class ProductCategoryMappingsApiServiceTests_GetMappingsCount
     {
-        [Test]
-        public void GivenNonEmptyValidRepository_WhenCalledWithDefaultParameters_ShouldReturnRepositorySize()
+        private List<ProductCategory> _repo;
+
+        [SetUp]
+        public void Setup()
         {
-            var repo = new List<ProductCategory>();
+            _repo = new List<ProductCategory>();
 
             var randomNumber = new Random();
 
-            var currentRepoSeze = randomNumber.Next(1, 100);
+            var currentRepoSize = randomNumber.Next(10, 100);
 
-            for (int i = 0; i < currentRepoSeze; i++)
+            _repo.Add(new ProductCategory()
             {
-                repo.Add(new ProductCategory()
+                CategoryId = 1,
+                ProductId = 2,
+            });
+
+            _repo.Add(new ProductCategory()
+            {
+                CategoryId = 2,
+                ProductId = 1,
+            });
+
+            for (int i = 0; i < currentRepoSize; i++)
+            {
+                _repo.Add(new ProductCategory()
                 {
-                    CategoryId = randomNumber.Next(1, 100),
-                    ProductId = randomNumber.Next(1, 100),
+                    CategoryId = 1,
+                    ProductId = randomNumber.Next(1, 2),
+                });
+
+                _repo.Add(new ProductCategory()
+                {
+                    CategoryId = 2,
+                    ProductId = randomNumber.Next(1, 2),
                 });
             }
+        }
 
+        [Test]
+        public void GivenNonEmptyValidRepository_WhenCalledWithDefaultParameters_ShouldReturnRepositorySize()
+        {
             // Arange
             var mappingRepo = MockRepository.GenerateStub<IRepository<ProductCategory>>();
-            mappingRepo.Stub(x => x.TableNoTracking).Return(repo.AsQueryable());
+            mappingRepo.Stub(x => x.TableNoTracking).Return(_repo.AsQueryable());
 
             // Act
             var cut = new ProductCategoryMappingsApiService(mappingRepo);
@@ -40,7 +64,7 @@ namespace Nop.Plugin.Api.Tests.ServicesTests.ProductCategoryMappings.GetMappings
             int result = cut.GetMappingsCount();
 
             // Assert
-            Assert.AreEqual(currentRepoSeze, result);
+            Assert.AreEqual(_repo.Count, result);
         }
 
         [Test]
@@ -68,26 +92,11 @@ namespace Nop.Plugin.Api.Tests.ServicesTests.ProductCategoryMappings.GetMappings
         [TestCase(2, 2)]
         public void GivenNonEmptyValidRepository_WhenCalledWithSomeParameters_ShouldReturnCountOfAllItemsAccordingToParameters(int categoryId, int productId)
         {
-            var repo = new List<ProductCategory>();
-
-            var randomNumber = new Random();
-
-            var currentRepoSize = randomNumber.Next(10, 100);
-
-            for (int i = 0; i < currentRepoSize; i++)
-            {
-                repo.Add(new ProductCategory()
-                {
-                    CategoryId = randomNumber.Next(1, 2),
-                    ProductId = randomNumber.Next(1, 2),
-                });
-            }
-
-            var expectedCount = repo.Count(x => x.CategoryId == categoryId && x.ProductId == productId);
+            var expectedCount = _repo.Count(x => x.CategoryId == categoryId && x.ProductId == productId);
 
             // Arange
             var mappingRepo = MockRepository.GenerateStub<IRepository<ProductCategory>>();
-            mappingRepo.Stub(x => x.TableNoTracking).Return(repo.AsQueryable());
+            mappingRepo.Stub(x => x.TableNoTracking).Return(_repo.AsQueryable());
 
             // Act
             var cut = new ProductCategoryMappingsApiService(mappingRepo);
@@ -103,26 +112,11 @@ namespace Nop.Plugin.Api.Tests.ServicesTests.ProductCategoryMappings.GetMappings
         [TestCase(2)]
         public void GivenNonEmptyValidRepository_WhenCalledWithCategoryIdParameter_ShouldReturnCountOfAllItemsThatAreMappedToThisCategory(int categoryId)
         {
-            var repo = new List<ProductCategory>();
-
-            var randomNumber = new Random();
-
-            var currentRepoSize = randomNumber.Next(10, 100);
-
-            for (int i = 0; i < currentRepoSize; i++)
-            {
-                repo.Add(new ProductCategory()
-                {
-                    CategoryId = randomNumber.Next(1, 2),
-                    ProductId = randomNumber.Next(10, 20),
-                });
-            }
-
-            var expectedCount = repo.Count(x => x.CategoryId == categoryId);
+            var expectedCount = _repo.Count(x => x.CategoryId == categoryId);
 
             // Arange
             var mappingRepo = MockRepository.GenerateStub<IRepository<ProductCategory>>();
-            mappingRepo.Stub(x => x.TableNoTracking).Return(repo.AsQueryable());
+            mappingRepo.Stub(x => x.TableNoTracking).Return(_repo.AsQueryable());
 
             // Act
             var cut = new ProductCategoryMappingsApiService(mappingRepo);
@@ -138,26 +132,11 @@ namespace Nop.Plugin.Api.Tests.ServicesTests.ProductCategoryMappings.GetMappings
         [TestCase(2)]
         public void GivenNonEmptyValidRepository_WhenCalledWithProductIdParameter_ShouldReturnCountOfAllItemsThatAreMappedToThisProduct(int productId)
         {
-            var repo = new List<ProductCategory>();
-
-            var randomNumber = new Random();
-
-            var currentRepoSize = randomNumber.Next(10, 100);
-
-            for (int i = 0; i < currentRepoSize; i++)
-            {
-                repo.Add(new ProductCategory()
-                {
-                    CategoryId = randomNumber.Next(10, 20),
-                    ProductId = randomNumber.Next(1, 2),
-                });
-            }
-
-            var expectedCount = repo.Count(x => x.ProductId == productId);
+            var expectedCount = _repo.Count(x => x.ProductId == productId);
 
             // Arange
             var mappingRepo = MockRepository.GenerateStub<IRepository<ProductCategory>>();
-            mappingRepo.Stub(x => x.TableNoTracking).Return(repo.AsQueryable());
+            mappingRepo.Stub(x => x.TableNoTracking).Return(_repo.AsQueryable());
 
             // Act
             var cut = new ProductCategoryMappingsApiService(mappingRepo);
