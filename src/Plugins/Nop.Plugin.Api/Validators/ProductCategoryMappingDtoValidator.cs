016af04a16b8447742951f2abeb8228050a3335d
itanev <iliyan.tanev@gmail.com>
2016-07-20 14:26:45 +0300

    Services logic removed from the product category mappings validator. Now it is in the controller.

--- src/Plugins/Nop.Plugin.Api/Validators/ProductCategoryMappingDtoValidator.cs
+++ src/Plugins/Nop.Plugin.Api/Validators/ProductCategoryMappingDtoValidator.cs
@@ -1,18 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using FluentValidation;
-using Nop.Core.Infrastructure;
 using Nop.Plugin.Api.DTOs.ProductCategoryMappings;
-using Nop.Services.Catalog;
 
 namespace Nop.Plugin.Api.Validators
 {
     public class ProductCategoryMappingDtoValidator : AbstractValidator<ProductCategoryMappingDto>
     {
-        private readonly ICategoryService _categoryService = EngineContext.Current.Resolve<ICategoryService>();
-        private readonly IProductService _productService = EngineContext.Current.Resolve<IProductService>();
-
-
         public ProductCategoryMappingDtoValidator(string httpMethod, Dictionary<string, object> passedPropertyValuePaires)
         {
             if (string.IsNullOrEmpty(httpMethod) || httpMethod.Equals("post", StringComparison.InvariantCultureIgnoreCase))
@@ -24,27 +18,7 @@ namespace Nop.Plugin.Api.Validators
                     {
                         mapping.RuleFor(a => a.ProductId)
                             .Must(productId => productId > 0)
-                            .WithMessage("invalid product_id")
-                            .DependentRules(c =>
-                            {
-                                c.RuleFor(d => d)
-                                    .Must(d => _categoryService.GetCategoryById((int)d.CategoryId) != null)
-                                    .WithMessage("category does not exist")
-                                    .DependentRules(d =>
-                                    {
-                                        d.RuleFor(e => e)
-                                            .Must(e => _productService.GetProductById((int)e.ProductId) != null)
-                                            .WithMessage("product does not exist")
-                                            .DependentRules(f =>
-                                            {
-                                                f.RuleFor(b => b)
-                                                    .Must(b =>
-                                                        _categoryService.GetProductCategoriesByCategoryId(
-                                                            (int)b.CategoryId, showHidden: true).FindProductCategory((int)b.ProductId, (int)b.CategoryId) == null)
-                                                    .WithMessage("mapping already exist");
-                                            });
-                                    });
-                            });
+                            .WithMessage("invalid product_id");
                     });
             }
             else if (httpMethod.Equals("put", StringComparison.InvariantCultureIgnoreCase))
@@ -59,26 +33,14 @@ namespace Nop.Plugin.Api.Validators
                 {
                     RuleFor(mapping => mapping.CategoryId)
                         .Must(categoryId => categoryId > 0)
-                        .WithMessage("category_id invalid")
-                        .DependentRules(mapping =>
-                        {
-                            mapping.RuleFor(m => m.CategoryId)
-                                .Must(categoryId => _categoryService.GetCategoryById((int)categoryId) != null)
-                                .WithMessage("category does not exist");
-                        });
+                        .WithMessage("category_id invalid");
                 }
 
                 if (passedPropertyValuePaires.ContainsKey("product_id"))
                 {
                     RuleFor(mapping => mapping.ProductId)
                         .Must(productId => productId > 0)
-                        .WithMessage("product_id invalid")
-                        .DependentRules(mapping =>
-                         {
-                             mapping.RuleFor(m => m.ProductId)
-                                    .Must(productId => _productService.GetProductById((int)productId) != null)
-                                    .WithMessage("product does not exist");
-                         });
+                        .WithMessage("product_id invalid");
                 }
             }
         }
